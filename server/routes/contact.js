1d4c1abf438be0ce9061ed0cd835dba43f154e3f
malldonado <matheussilvacontat@gmail.com>
2024-08-28 11:00:40 -0300

    update project

--- server/routes/contact.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getContactData, postContactData, deleteContactData } = require('../controllers/contact');
-const router = express.Router();
-
-router.get('/contact', getContactData);
-router.post('/contact', postContactData);
-router.delete('/contact/:id', deleteContactData);
-
-module.exports = router;
